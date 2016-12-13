8b5461e0f11f4e7589640516ca4ef00a87b59a6b
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-12-13 15:59:24 +0100

    constructor & collection in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private title: string;
+  private version: string;
+  private collection: [any];
+
+  constructor() {
+    this.title = 'Delivery App';
+    this.version = '1.0';
+    this.collection = [
+      {reference: '1234', name: 'anton'},
+      {reference: '5678', name: 'levon'},
+      {reference: '9012', name: 'dor'}
+    ]
+  }
 }
