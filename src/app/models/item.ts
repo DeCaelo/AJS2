73b228fcc4992298ca58dd33c4a5d2132e4670d3
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-12-15 09:21:03 +0100

    animatestate added

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -1,11 +1,18 @@
 export class Item {
-  private reference: string;
-  private name: string;
-  private state: number;
+  public reference: string;
+  public name: string;
+  public state: number;
+  public animateState: string;
 
   constructor(data: any) {
     this.reference = data.reference;
     this.name = data.name;
     this.state = data.state;
+    this.animateState = 'inactive';
+  }
+
+  toggleAnimateState() {
+    this.animateState = this.animateState === 'inactive' ? 'active' : 'inactive';
+    console.log(this.animateState);
   }
 }
