fd5a1b55aaf0cf24611b70ad332a4db9cb5ce065
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-12-16 15:56:57 +0100

    link service with module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { Item } from './models/item';
 import { Config } from './config';
+import { ItemService } from './services/item.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ItemService]
 })
 export class AppComponent {
   private title: string;
   private version: string;
-  private collection: [Item];
+  private collection: Item[];
+  private ItemService: ItemService;
 
-  constructor() {
+  constructor(ItemService: ItemService) {
     this.title = Config.APP_TITLE;
     this.version = Config.APP_VERSION;
+    this.ItemService = ItemService;
+    this.collection = ItemService.getCollection();
   }
 
   onCreateItem(item: Item) {
-    this.collection.unshift(item);
+    this.ItemService.addItemToCollection(item);
   }
 }
