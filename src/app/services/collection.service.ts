dd64c66688de846ece9f1bbd97c5c771c4c64835
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-12-20 09:43:13 +0100

    putting call & responding

--- src/app/services/collection.service.ts
+++ src/app/services/collection.service.ts
@@ -4,36 +4,40 @@ import { Item } from '../models/Item';
 import { Http, Response } from '@angular/http';
 import { Observable }  from 'rxjs/Observable';
 
+import {SocketService} from './socket.service';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CollectionService {
     public collection: Item[];
     private Http: Http;
+    private socket: any;
     @Output() update: EventEmitter<Item[]> = new EventEmitter();
 
-    constructor(Http: Http) {
+    constructor(Http: Http, SocketService: SocketService) {
         this.Http = Http;
+        this.socket = SocketService.socket();
+        let that = this;
+
+        this.socket.on('collection', function(collection: Item[]) {
+            that.collection = collection;
+            that.updateCollection();
+        })
     }
 
     /**
      *
      */
     getCollection(): any {
-        this.Http.get(Config.API_ROUTES.collection).map(response => {
-            this.collection = response.json();
-            this.updateCollection();
-        }).subscribe();
+        this.socket.emit('collection:get');
     }
 
     /**
      *
      */
     addItemToCollection(item: Item): any {
-        this.Http.post(Config.API_ROUTES.collection, item).map(response => {
-            this.collection = response.json();
-            this.updateCollection();
-        }).subscribe();
+        this.socket.emit('collection:add', item);
     }
 
     updateCollection() {
